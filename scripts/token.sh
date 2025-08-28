511d7d5845c48fe40758a35bf2ababd680a2d9a6
Jack Gilcrest <me@jp4g.llc>
2025-08-28 14:22:51 -0600

    test utilities setup

--- scripts/token.sh
+++ scripts/token.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+
+# compile token contract
 git submodule update --init --recursive
 cd deps/aztec-standards
 aztec-nargo compile --package token_contract
-aztec codegen ./target/token_contract-Token.json -o ../../packages/contracts/artifacts -f
+
+# generate TypeScript bindings
+aztec codegen ./target/token_contract-Token.json \
+    -o ../../packages/contracts/artifacts -f
+
+# add the artifact to the target file for txe
+if [ ! -d "../../packages/contracts/target" ]; then
+    mkdir ../../packages/contracts/target
+fi
+cp ./target/token_contract-Token.json ../../packages/contracts/target
 
