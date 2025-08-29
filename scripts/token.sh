d7169e23d1b80216564170d3d6eab04c93a20b1f
Jack Gilcrest <me@jp4g.llc>
2025-08-28 18:15:29 -0600

    all reasonable tests at least i can think of + prevent from double deposit

--- scripts/token.sh
+++ scripts/token.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
+# if -r flag is provided, skip download of submodule and only recompile
+SKIP_SUBMODULES=false
+while getopts "r" opt; do
+  case $opt in
+    r)
+      SKIP_SUBMODULES=true
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+if [ "$SKIP_SUBMODULES" = false ]; then
+    # Only run submodule update if -r flag is not provided
+    git submodule update --init --recursive
+else
+    # Remove target if rebuilding
+    rm -rf deps/aztec-standards/target
+fi
+
 # compile token contract
-git submodule update --init --recursive
 cd deps/aztec-standards
 aztec-nargo compile --package token_contract
 
