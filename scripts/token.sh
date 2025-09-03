161734548100ec694d486da61f5ed1620bb360e6
Jack Gilcrest <me@jp4g.llc>
2025-09-03 13:00:19 -0600

    switch script flag for recompile

--- scripts/token.sh
+++ scripts/token.sh
@@ -2,13 +2,13 @@
 
 # if -r flag is provided, skip download of submodule and only recompile
 SKIP_SUBMODULES=false
-while getopts "r" opt; do
-  case $opt in
-    r)
+for arg in "$@"; do
+  case $arg in
+    --skip-submodules)
       SKIP_SUBMODULES=true
       ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
+    *)
+      echo "Invalid option: $arg" >&2
       exit 1
       ;;
   esac
