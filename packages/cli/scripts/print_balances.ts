be702aace915d19e7dcedb0d9382f4daff4d83a6
Jack Gilcrest <gilcrest.jack@gmail.com>
2025-11-01 14:03:04 -0600

    fix balance script

--- packages/cli/scripts/print_balances.ts
+++ packages/cli/scripts/print_balances.ts
@@ -20,60 +20,52 @@ const main = async () => {
     const { wallet, sellerAddress, buyerAddress } = await getOTCAccounts(node);
 
     // get tokens
-    // const ethAddress = AztecAddress.fromString(ethDeployment.address);
-    // const eth = await getTokenContract(sellerWallet, sellerAddress, node, ethAddress);
+    const ethAddress = AztecAddress.fromString(ethDeployment.address);
+    const eth = await getTokenContract(wallet, sellerAddress, node, ethAddress);
 
-    // const usdcAddress = AztecAddress.fromString(usdcDeployment.address);
-    // const usdc = await getTokenContract(sellerWallet, sellerAddress, node, usdcAddress);
+    const usdcAddress = AztecAddress.fromString(usdcDeployment.address);
+    const usdc = await getTokenContract(wallet, sellerAddress, node, usdcAddress);
 
-    // // check balances for seller
-    // const sellerETHBalance = await eth
-    //     .withWallet(sellerWallet)
-    //     .methods
-    //     .balance_of_private(sellerAddress)
-    //     .simulate({ from: sellerAddress });
-    // const sellerUSDCBalance = await usdc
-    //     .withWallet(sellerWallet)
-    //     .methods
-    //     .balance_of_private(sellerAddress)
-    //     .simulate({ from: sellerAddress });
+    // check balances for seller
+    const sellerETHBalance = await eth
+        .withWallet(wallet)
+        .methods
+        .balance_of_private(sellerAddress)
+        .simulate({ from: sellerAddress });
+    const sellerUSDCBalance = await usdc
+        .withWallet(wallet)
+        .methods
+        .balance_of_private(sellerAddress)
+        .simulate({ from: sellerAddress });
     
-    // // add tokens to buyer wallet
-    // await buyerWallet.registerContract(eth);
-    // await eth
-    //     .withWallet(buyerWallet)
-    //     .methods
-    //     .sync_private_state();
-    // await buyerWallet.registerContract(usdc);
-    // await usdc
-    //     .withWallet(buyerWallet)
-    //     .methods
-    //     .sync_private_state();
-    // const buyerETHBalance = await eth
-    //     .withWallet(buyerWallet)
-    //     .methods
-    //     .balance_of_private(buyerAddress)
-    //     .simulate({ from: buyerAddress });
-    // const buyerUSDCBalance = await usdc
-    //     .withWallet(buyerWallet)
-    //     .methods
-    //     .balance_of_private(buyerAddress)
-    //     .simulate({ from: buyerAddress });
+    // add tokens to buyer wallet
+    await wallet.registerContract(eth);
+    await eth
+        .withWallet(wallet)
+        .methods
+        .sync_private_state();
+    await wallet.registerContract(usdc);
+    await usdc
+        .withWallet(wallet)
+        .methods
+        .sync_private_state();
+    const buyerETHBalance = await eth
+        .withWallet(wallet)
+        .methods
+        .balance_of_private(buyerAddress)
+        .simulate({ from: buyerAddress });
+    const buyerUSDCBalance = await usdc
+        .withWallet(wallet)
+        .methods
+        .balance_of_private(buyerAddress)
+        .simulate({ from: buyerAddress });
 
-    // if testnet, check available funds
-    if (await isTestnet(node)) {
-        // todo: WE NEED TO SHIELD FEE PAYING
-        const feeJuiceBalanceSeller = await getFeeJuicePublicBalance(node, sellerAddress);
-        const feeJuiceBalanceBuyer = await getFeeJuicePublicBalance(node, buyerAddress);
-        console.log(`FeeJuice balance for seller: ${feeJuiceBalanceSeller}`);
-        console.log(`FeeJuice balance for buyer: ${feeJuiceBalanceBuyer}`);
-    }
-    // console.log("==================[Balances]==================")
-    // console.log(`ETH balance for seller: ${sellerETHBalance}`);
-    // console.log(`USDC balance for seller: ${sellerUSDCBalance}`);
-    // console.log(`ETH balance for buyer: ${buyerETHBalance}`);
-    // console.log(`USDC balance for buyer: ${buyerUSDCBalance}`);
-    // console.log("==============================================");
+    console.log("==================[Balances]==================")
+    console.log(`ETH balance for seller: ${sellerETHBalance}`);
+    console.log(`USDC balance for seller: ${sellerUSDCBalance}`);
+    console.log(`ETH balance for buyer: ${buyerETHBalance}`);
+    console.log(`USDC balance for buyer: ${buyerUSDCBalance}`);
+    console.log("==============================================");
 }
 
 main();
